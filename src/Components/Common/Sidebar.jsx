6ce9a4f8b5b8c516e231c0ff3dbad72aa07e5437
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-17 19:37:26 +0330

    adds filtered questions to filter section of redux

--- src/Components/Common/Sidebar.jsx
+++ src/Components/Common/Sidebar.jsx
@@ -1,18 +1,31 @@
+import axios from "axios";
 import { useCallback } from "react";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { SET_FILTERED_QUESTIONS } from "../../Store/entities/filters";
 import HardnessFilter from "../HardnessFilter";
 import LessonFilter from "../LessonFilter";
 import Btn from "./Btn";
 
 function Sidebar() {
+  const dispatch = useDispatch();
+
   const { subject, hardness, searchQuery } = useSelector(
     (store) => store.entities.filters
   );
   const applyAllFilters = useCallback(() => {
     const numbericHardness = Number(hardness);
-    console.log(subject, numbericHardness, searchQuery);
-  }, [hardness, searchQuery, subject]);
+    axios
+      .get("/questions/", {
+        params: {
+          q: searchQuery,
+          subject,
+          level: numbericHardness,
+        },
+      })
+      .then((res) => dispatch(SET_FILTERED_QUESTIONS({ questions: res.data })))
+      .catch((err) => console.log(err.response));
+  }, [dispatch, hardness, searchQuery, subject]);
 
   return (
     <div className="bg-stone-900 px-5 py-2 h-screen text-white">
