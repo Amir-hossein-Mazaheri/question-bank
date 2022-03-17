27714f86f62ff760b707582a3b4e82f551a5e618
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-17 18:20:42 +0330

    added filtered questions section in filter slice of redux store

--- src/Store/entities/filters.js
+++ src/Store/entities/filters.js
@@ -3,15 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 const filters = createSlice({
   name: "filters",
   initialState: {
-    sortBy: "all",
     subject: "",
     hardness: "all", // all for no filter 1 for easy 2 for medium 3 for hard
     searchQuery: "",
+    filteredQuestions: [],
   },
   reducers: {
-    SET_SORT: (store, action) => {
-      store.sortBy = action.payload.sortType;
-    },
     SET_HARDNESS: (store, action) => {
       store.hardness = action.payload.hardness;
     },
@@ -21,10 +18,18 @@ const filters = createSlice({
     SET_SEARCH_QUERY: (store, action) => {
       store.searchQuery = action.payload.query;
     },
+    SET_FILTERED_QUESTIONS: (store, action) => {
+      store.filteredQuestions = action.payload.questions;
+    },
   },
 });
 
 export default filters.reducer;
 
-export const { SET_SORT, SET_HARDNESS, SET_SUBJECT, SET_SEARCH_QUERY } =
-  filters.actions;
+export const {
+  SET_SORT,
+  SET_HARDNESS,
+  SET_SUBJECT,
+  SET_SEARCH_QUERY,
+  SET_FILTERED_QUESTIONS,
+} = filters.actions;
