6755b53880c0a4545ef205462c7ee40b893b4fda
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-20 15:17:52 +0330

    more accurate token verification

--- src/Components/Common/Sidebar.jsx
+++ src/Components/Common/Sidebar.jsx
@@ -5,6 +5,7 @@ import { FORCE_TO_UPDATE, SET_URL_PARAMS } from "../../Store/entities/filters";
 import HardnessFilter from "../HardnessFilter";
 import LessonFilter from "../LessonFilter";
 import Btn from "./Btn";
+import Auth from "../../Helpers/Auth";
 
 function Sidebar() {
   const dispatch = useDispatch();
@@ -25,21 +26,34 @@ function Sidebar() {
     dispatch(FORCE_TO_UPDATE());
   }, [dispatch, hardness, searchQuery, subject]);
 
+  const logOut = useCallback(() => {
+    Auth.logout();
+    window.location.replace("http://lapluse.ir/exam-login/");
+  }, []);
+
   return (
     <div className="bg-stone-900 px-5 py-2 h-screen text-white">
-      <HardnessFilter />
-      <LessonFilter />
-      <Btn
-        className="w-full block text-black mt-7 bg-white"
-        onClick={applyAllFilters}
-      >
-        اعمال فیلتر ها
-      </Btn>
+      <div>
+        <HardnessFilter />
+        <LessonFilter />
+        <Btn
+          className="w-full block text-black mt-7 bg-white"
+          onClick={applyAllFilters}
+        >
+          اعمال فیلتر ها
+        </Btn>
 
-      <div className="bg-red-300 px-7 py-5 rounded-md mt-5">
-        <h2 className="font-bold leading-relaxed text-red-500">
-          توجه : برای اعمال جستجو نیز باید بر روی دکمه اعمال فیلتر کلیک شود.
-        </h2>
+        <div className="bg-red-300 px-7 py-5 rounded-md mt-5">
+          <h2 className="font-bold leading-relaxed text-red-500">
+            توجه : برای اعمال جستجو نیز باید بر روی دکمه اعمال فیلتر کلیک شود.
+          </h2>
+        </div>
+      </div>
+
+      <div>
+        <Btn onClick={logOut} className="bg-red-500 text-white w-full py-3">
+          <span>خروج</span>
+        </Btn>
       </div>
     </div>
   );
