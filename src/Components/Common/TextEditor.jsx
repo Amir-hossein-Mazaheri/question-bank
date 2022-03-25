038cfcac060779e03e92d3af4ae6e62fb629cd71
LightSpeedGuy <uplayggboy@gmail.com>
2022-03-25 09:12:12 +0430

    fixed upload url

--- src/Components/Common/TextEditor.jsx
+++ src/Components/Common/TextEditor.jsx
@@ -8,7 +8,7 @@ function TextEditor({ className, ...others }) {
         editor={Editor}
         config={{
           ckfinder: {
-            uploadUrl: "http://lapluse.ir/examapi/ckeditor/",
+            uploadUrl: "http://lapluse.ir/examapi/ckeditor/upload/",
           },
         }}
         {...others}
